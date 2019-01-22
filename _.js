05348dce7d3cd4a82cdd07c8256599940901244e
nunezisrael <nunezisrael@gmail.com>
2019-01-22 14:02:43 -0500

    Update _.js

--- _.js
+++ _.js
@@ -1,7 +1,6 @@
 let _ = {
-
+//Clamp
   clamp: function(number, lower, upper){
-
   if(number < lower){
     return lower
   }
@@ -12,6 +11,7 @@ let _ = {
     return number
     }
   },
+//in-inRange
   inRange(number, start, end){
   let newStart, newEnd;
   const checkForNoEnd = endVal =>{
@@ -30,7 +30,6 @@ let _ = {
   }
   checkForNoEnd(end)
   checkForSwap(start,end)
-
   if(number < start){
     return false
   }
@@ -41,10 +40,12 @@ let _ = {
     return true
   }
 },
+//Words
   words(string){
     let wordArr = string.split(' ')
     return wordArr
   },
+//Pad
   pad: function(string,length){
     let padding = 0, extraChar = 0, amount = length - string.length
     let paddedString = ''
@@ -64,19 +65,34 @@ let _ = {
         return padding, extraChar, amount
       }
     }
-
     padAmount()
     paddedString = ' '.repeat(padding)+string+' '.repeat(padding)+' '.repeat(extraChar)
     return paddedString
   },
+//Has
   has(object,key){
   for(let pkey in object){
     console.log(object)
     console.log(object.key)
     return pkey === key ? true : false
     }
- },
- 
-}
+  },
+//invert function
+  invert(object){
+    console.log('original Object: ', object)
+    let newObject = {}
+      for (let key in object){
+        let newKey = '', newValue = ''
+        newValue = key
+        newKey = object[key]
+        newObject[newKey] = newValue
+        }
+      return newObject
+    }
+  },
+//findKey function
+  findKey(objecy,predicate){
+
+   }
 //// Do not write or modify code below this line.
 module.exports = _;
