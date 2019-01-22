064109d023ce5e5faf3b4f4a99ccbc1f510fbb08
Israel Nunez <nunezisrael@gmail.com>
2019-01-21 21:00:39 -0500

    added invert
    passed testing

--- _.js
+++ _.js
@@ -1,7 +1,21 @@
+let _ = {
 
-
-
-
-
+//invert function
+invert(object){
+  console.log('original Object: ', object)
+  let newObject = {}
+    for (let key in object){
+      let newKey = '', newValue = ''
+      newValue = key
+      newKey = object[key]
+      newObject[newKey] = newValue
+    }
+    return newObject
+  },
+//findKey function
+findKey(objecy,predicate){
+  
+ }
+}
 // Do not write or modify code below this line.
 module.exports = _;
---
+++ objectTest.js
@@ -0,0 +1,8 @@
+const person = {
+firstName: 'Joe',
+lastName: 'Smith',
+age: 56,
+gender: 'male'
+}
+
+console.log(person.firstName)  
